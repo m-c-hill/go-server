80a70f2e293f874d458bf0454fc20d787c08b942
m-c-hill <matthewhill2595@gmail.com>
2022-04-10 13:08:01 +0100

    Add request and respond methods for TCP server

--- main.go
+++ main.go
@@ -5,40 +5,69 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
+	"strings"
 )
 
 func main() {
 	// Listen, accept, read/write
 	li, err := net.Listen("tcp", ":8080")
-
 	if err != nil {
-		log.Panic(err)
+		log.Fatalln(err.Error())
 	}
 	defer li.Close()
 
 	for {
 		conn, err := li.Accept()
 		if err != nil {
-			log.Println(err)
+			log.Fatalln(err.Error())
 		}
 		go handle(conn)
 	}
 }
 
 func handle(conn net.Conn) {
-	// Close connection after 10 seconds
-	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
-	if err != nil {
-		log.Println("CONN TIMEOUT")
-	}
+	defer conn.Close()
+
+	// read request
+	request(conn)
+
+	// write response
+	respond(conn)
+}
+
+// HTTP specification: https://www.w3.org/Protocols/rfc2616/rfc2616.html
+
+func request(conn net.Conn) {
+	i := 0
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		ln := scanner.Text()
 		fmt.Println(ln)
-		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
-	}
-	defer conn.Close()
+		if i == 0 {
+			// Request line
+			m := strings.Fields(ln)[0]
+			fmt.Println("***METHOD", m)
 
-	fmt.Println("Code got here.")
+			u := strings.Fields(ln)[1]
+			fmt.Println("***URL", u)
+
+			fmt.Println(ln)
+		}
+		if ln == "" {
+			// Blank line after request line and headers
+			break
+		}
+		i++
+	}
+
+}
+
+func respond(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta charet="UTF-8"><title><\title><\head><body><strong>Hello World</strong></body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Lenght: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
